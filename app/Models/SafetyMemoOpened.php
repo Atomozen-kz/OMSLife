8eef3ae69ea5010dd7473f224d78dc2a1a29c26c
nurik-atom <baliikhanov@gmail.com>
2026-02-04 11:09:59 +0500

    Safety Memos opened

--- app/Models/SafetyMemoOpened.php
+++ app/Models/SafetyMemoOpened.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SafetyMemoOpened extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'safety_memo_opened';
+
+    protected $fillable = [
+        'safety_memo_id',
+        'sotrudnik_id',
+    ];
+
+    /**
+     * Связь с памяткой безопасности
+     */
+    public function safetyMemo()
+    {
+        return $this->belongsTo(SafetyMemo::class, 'safety_memo_id');
+    }
+
+    /**
+     * Связь с сотрудником
+     */
+    public function sotrudnik()
+    {
+        return $this->belongsTo(Sotrudniki::class, 'sotrudnik_id');
+    }
 }
